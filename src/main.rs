b250c122301c599379409fd0405d83b42e14813d
Rh4096 <rhelvetican@gmail.com>
2024-12-03 18:05:54 +0700

    day 3

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,7 @@
-fn main() {}
+use aoc2024::solutions::{day_3::AocDayThreeSolution, AocSolution};
+
+fn main() {
+    let sol = AocDayThreeSolution;
+
+    println!("p1: {}\np2: {}", sol.part_one(), sol.part_two())
+}
---
+++ src/solutions/day_3.rs
@@ -0,0 +1,51 @@
+use std::sync::LazyLock;
+
+use regex::Regex;
+
+use super::AocSolution;
+
+static MUL: LazyLock<Regex> =
+    LazyLock::new(|| Regex::new(r"mul\([0-9]{1,3},[0-9]{1,3}\)").unwrap());
+
+static COND_MUL: LazyLock<Regex> =
+    LazyLock::new(|| Regex::new(r"(mul\([0-9]{1,3},[0-9]{1,3}\))|(do\(\))|(don't\(\))").unwrap());
+
+#[derive(Clone, Copy)]
+pub struct AocDayThreeSolution;
+
+fn as_mul(s: &str) -> u64 {
+    let (a, b) = s[4..(s.len() - 1)]
+        .split_once(',')
+        .map(|(a, b)| (a.parse::<u64>().unwrap(), b.parse::<u64>().unwrap()))
+        .unwrap();
+
+    a * b
+}
+
+impl AocSolution for AocDayThreeSolution {
+    type Output = u64;
+    const INPUT: &str = include_str!("../../input/day3.txt");
+
+    fn part_one(&self) -> Self::Output {
+        MUL.find_iter(Self::INPUT)
+            .map(|x| x.as_str())
+            .map(as_mul)
+            .sum()
+    }
+
+    fn part_two(&self) -> Self::Output {
+        COND_MUL
+            .find_iter(Self::INPUT)
+            .map(|x| x.as_str())
+            .fold((0, true), |(sum, flag), cut| {
+                if cut.starts_with("mul") && flag {
+                    (sum + as_mul(cut), flag)
+                } else if cut == "do()" {
+                    (sum, true)
+                } else {
+                    (sum, false)
+                }
+            })
+            .0
+    }
+}
